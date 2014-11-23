96a60f57a2caf04cadd7c75e21392d5d562f9568
Eleanor Weigert <eweigert@gmail.com>
2014-11-23 10:38:08 -0800

    Added another solution

---
+++ TwoDWalk.java
@@ -0,0 +1,28 @@
+public class TwoDWalk {
+				public static void main (String[] args){
+				int N = Integer.parseInt(args[0]);
+				int x = N;
+				int y = N;
+				int totalMoves = 0;
+				while((x != 0 && x!= 2*N) && (y != 0 && y!= 2*N)){
+					double move = Math.random();
+					if(move < 0.25){
+					y -= 1;
+					totalMoves++;
+					}
+					if(move >= 0.25 && move < 0.5){
+					y += 1;
+					totalMoves++;
+					}
+					if(move >= 0.5 && move < 0.75) {
+					x += 1;
+					totalMoves++;
+					}
+					if(move >= 0.75){
+					x -= 1;
+					totalMoves++;
+					}
+				}
+				System.out.println("After " +totalMoves +" moves, the random walker hit a boundary.");
+				}
+}
