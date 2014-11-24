995c235f4265bd5b2383d3f108b0de1c2cda9894
Eleanor Weigert <eweigert@gmail.com>
2014-11-23 21:05:19 -0800

    My Deal Game is not the same as the real one! Proceed with caution

--- DealGame.java
+++ DealGame.java
@@ -1,17 +1,15 @@
 public class DealGame {
 			public static void main(String[] args){
-				//tests whether or not it's advantage to switch doors in "Let's make a deal!""
+				//tests whether or not it's advantagous to switch doors in "Let's make a deal!"  Unlike the real game, this simulation allows for the possibility that the opened door could have been the door with the big prize.
 				int N = Integer.parseInt(args[0]);
 				//number of the game should be simulated to test each strategy (switch or don't switch)
 				double dontSwitch = 0;
 				double doSwitch = 0;
 				double newChoice = 0;
-				
+	
 				for(int i = 0; i < N; i++){
-					double chosenDoor = Math.round(Math.random() * 2);
-					double prizeDoor = Math.round(Math.random() * 2);
-					System.out.println("chosen door: " +chosenDoor);
-					System.out.println("prize door: " +prizeDoor);
+					double chosenDoor = (int)(Math.random() * 2);
+					double prizeDoor = (int)(Math.random() * 2);
 
 					if(chosenDoor == prizeDoor) {
 						dontSwitch++;
@@ -19,16 +17,16 @@ public class DealGame {
 
 					newChoice = Math.round(Math.random() * 2);
 					while(newChoice == chosenDoor){ 
-						newChoice = Math.round(Math.random() * 2);
+						newChoice = (int)(Math.random() * 2);
 						}
 					if(newChoice == prizeDoor) doSwitch++;	
-					System.out.println("new choice: " +newChoice);				
+						
 				}
 					
 					
 				double successSwitch = Math.round((doSwitch / N) * 100);
 				double successStay = Math.round((dontSwitch / N) * 100);
-
+			
 				System.out.println("Out of " +N+" trials, your chances of success if you switch are: " +(int)successSwitch+ "% " + "versus " +(int)successStay+ "% chances of success for not switching.");
 }
 }
