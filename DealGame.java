ef04372b2ba74a36f3449e583c71adba1820f795
Eleanor Weigert <eweigert@gmail.com>
2014-11-23 20:45:57 -0800

    fixed DealGame

--- DealGame.java
+++ DealGame.java
@@ -6,23 +6,26 @@ public class DealGame {
 				double dontSwitch = 0;
 				double doSwitch = 0;
 				double newChoice = 0;
+				
 				for(int i = 0; i < N; i++){
-					double chosenDoor = Math.round(Math.random() * 10 * 0.3);
-					if(chosenDoor == 0) chosenDoor = 1;
-					double prizeDoor = Math.round(Math.random() * 10 * 0.3);
-					if(prizeDoor == 0) prizeDoor = 1;
+					double chosenDoor = Math.round(Math.random() * 2);
+					double prizeDoor = Math.round(Math.random() * 2);
+					System.out.println("chosen door: " +chosenDoor);
+					System.out.println("prize door: " +prizeDoor);
+
 					if(chosenDoor == prizeDoor) {
 						dontSwitch++;
 						}
-					newChoice = Math.round(Math.random() * 10 * 0.3);
-					if(newChoice == 0) newChoice = 1;
-					while(newChoice == chosenDoor){ 
-						newChoice = Math.round(Math.random() * 10 * 0.3);
-						if(newChoice == 0) newChoice = 1;
-				}
-					if(newChoice == prizeDoor) doSwitch++;
 
+					newChoice = Math.round(Math.random() * 2);
+					while(newChoice == chosenDoor){ 
+						newChoice = Math.round(Math.random() * 2);
+						}
+					if(newChoice == prizeDoor) doSwitch++;	
+					System.out.println("new choice: " +newChoice);				
 				}
+					
+					
 				double successSwitch = Math.round((doSwitch / N) * 100);
 				double successStay = Math.round((dontSwitch / N) * 100);
 
